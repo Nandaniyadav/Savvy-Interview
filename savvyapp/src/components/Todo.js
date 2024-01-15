a458e27e016c23313e64e2ee689746e19fffb271
Nandani Yadav <77967134+Nandaniyadav@users.noreply.github.com>
2024-01-15 12:17:36 +0530

    add check and delete icon

--- savvyapp/src/components/Todo.js
+++ savvyapp/src/components/Todo.js
@@ -1,6 +1,7 @@
 import {  CardContent, Card, Container, Typography, IconButton } from "@mui/material";
 import React from "react";
 import CheckIcon from '@mui/icons-material/Check';
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 
 const Todo = () => {
   return (
@@ -12,9 +13,14 @@ const Todo = () => {
         >
           <CardContent>
             <Typography variant="h5" component="h2">
-                <IconButton>
-               <CheckIcon style={{ color:"green"}}></CheckIcon> 
-                </IconButton>
+                {/* check icon button */}
+                <IconButton style={{float:"left"}}>
+               <CheckIcon style={{ color:"green"}}/>
+               </IconButton>
+               <IconButton style={{float:"right"}}>
+               <DeleteForeverIcon style={{color:"red"}}/>
+               </IconButton>
+                
             </Typography>
           </CardContent>
         </Card>
